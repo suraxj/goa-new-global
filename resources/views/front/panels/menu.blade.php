faf0b4c18666fce1bbfa6449fa1a322b7a3d7861
kumarpriyanshu9341-pixel <kumarpriyanshu9341@gmail.com>
2026-08-10 10:36:01 +0530

    Redesign mobile drawer menu header, fix email to admissions@apexhorizon.edu.in, and eliminate whitespace gap

--- resources/views/front/panels/menu.blade.php
+++ resources/views/front/panels/menu.blade.php
@@ -15,59 +15,25 @@
     </form>
 </div>
 <div class="th-menu-wrapper">
-    <div class="th-menu-area text-center">
-        <button class="th-menu-toggle"><i class="fal fa-times"></i></button>
-        <div class="th-menu-content">
-            <div class="mobile-logo">
-                <a href="/"><img src="/web-assets/img/logo-gga.svg" alt="Apex Horizon Institute" style="height: 52px;" /></a>
-            </div>
-            <div class="th-mobile-menu-bottom">
-                <form class="th-mobile-search" action="#">
-                    <input type="text" placeholder="Search..." />
-                    <button class="icon-btn" type="submit">
-                        <i class="fal fa-search"></i>
-                    </button>
-                </form>
-                <div class="btn-wrap">
-                    <a href="" data-bs-toggle="modal" data-bs-target="#pop" class="th-btn w-100">Registration Now
-                        <svg class="ms-2" width="16" height="14" viewBox="0 0 16 14" fill="none"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M7.5264 0C7.5264 0.6962 8.21633 1.738 8.9138 2.61293C9.81193 3.7394 10.8838 4.72347 12.1137 5.4748C13.0351 6.0374 14.154 6.57747 15.0528 6.57747M7.5264 13.1712C7.5264 12.475 8.21633 11.4332 8.9138 10.5583C9.81193 9.43187 10.8838 8.44773 12.1137 7.6964C13.0351 7.1338 14.154 6.59373 15.0528 6.59373M15.0528 6.5856H0"
-                                stroke="currentColor" stroke-width="1.5"></path>
-                        </svg></a>
-                </div>
-                <div class="contact-info-wrap">
-                    <div class="contact-info">
-                        <i class="fa-regular fa-envelope"></i> <a href="mailto:{{ $setting->primary_email ?? 'admissions@apexhorizon.edu.in' }}">
-                            {{ $setting->primary_email ?? 'admissions@apexhorizon.edu.in' }}
-                        </a>
-                    </div>
-                    <div class="contact-info">
-                        <i class="fa-regular fa-phone"></i><a href="tel:{{ $setting->primary_contact ?? '+919881788888' }}">
-                            {{ $setting->primary_contact ?? '+91 98817 88888' }}
-                        </a>
-                    </div>
-                </div>
-                <div class="th-social style4">
-                    <a href="#" title="Facebook"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#" title="Twitter"><i class="fab fa-twitter"></i></a>
-                    <a href="#" title="LinkedIn"><i class="fab fa-linkedin-in"></i></a>
-                    <a href="#" title="Instagram"><i class="fab fa-instagram"></i></a>
-                    <a href="#" title="YouTube"><i class="fab fa-youtube"></i></a>
-                </div>
-            </div>
-            <div class="th-mobile-menu">
+    <div class="th-menu-area text-start">
+        <div class="d-flex align-items-center justify-content-between p-3 border-bottom border-secondary border-opacity-10">
+            <a href="/" class="mobile-logo">
+                <img src="/web-assets/img/logo-gga.svg" alt="Apex Horizon Institute" style="max-height: 44px; max-width: 220px;" />
+            </a>
+            <button class="th-menu-toggle btn btn-sm btn-outline-dark rounded-circle p-0 d-flex align-items-center justify-content-center" style="width: 36px; height: 36px;" aria-label="Close Menu">
+                <i class="fal fa-times"></i>
+            </button>
+        </div>
+        <div class="th-menu-content p-3">
+            <div class="th-mobile-menu mb-4">
                 <ul>
-                    <li class="menu-item-has-children">
-                        <a href="/" class="active">Home</a>
-                    </li>
+                    <li class="active"><a href="/">Home</a></li>
                     <li><a href="/about">About Us</a></li>
                     <li class="menu-item-has-children">
                         <a href="#course-sec">Courses</a>
                         <ul class="sub-menu">
                             @php
-                                $unis = DB::select('select name, slug from courses order by name asc limit 5');
+                                $unis = DB::select('select name, slug from courses order by name asc limit 6');
                             @endphp
                             @foreach ($unis as $uni)
                                 <li><a href="/course/{{ $uni->slug }}">{{ $uni->name }}</a></li>
@@ -77,6 +43,35 @@
                     <li><a href="/blog">Blogs</a></li>
                     <li><a href="/contact">Contact Us</a></li>
                 </ul>
+            </div>
+
+            <div class="th-mobile-menu-bottom">
+                <div class="btn-wrap mb-3">
+                    <a href="" data-bs-toggle="modal" data-bs-target="#pop" class="btn btn-cyber-glow w-100 py-2.5 font-weight-bold">
+                        Registration Now &rarr;
+                    </a>
+                </div>
+                <div class="contact-info-wrap mb-3 p-3 rounded-12" style="background: rgba(79, 70, 229, 0.05); border: 1px solid rgba(79, 70, 229, 0.12);">
+                    <div class="contact-info mb-2 small font-weight-bold">
+                        <i class="fa-regular fa-envelope text-primary me-2"></i>
+                        <a href="mailto:{{ $setting->primary_email ?? 'admissions@apexhorizon.edu.in' }}" class="text-dark">
+                            {{ $setting->primary_email ?? 'admissions@apexhorizon.edu.in' }}
+                        </a>
+                    </div>
+                    <div class="contact-info small font-weight-bold">
+                        <i class="fa-regular fa-phone text-success me-2"></i>
+                        <a href="tel:{{ $setting->primary_contact ?? '+919881788888' }}" class="text-dark">
+                            {{ $setting->primary_contact ?? '+91 98817 88888' }}
+                        </a>
+                    </div>
+                </div>
+                <div class="th-social style4 d-flex gap-2">
+                    <a href="#" title="Facebook"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" title="Twitter"><i class="fab fa-twitter"></i></a>
+                    <a href="#" title="LinkedIn"><i class="fab fa-linkedin-in"></i></a>
+                    <a href="#" title="Instagram"><i class="fab fa-instagram"></i></a>
+                    <a href="#" title="YouTube"><i class="fab fa-youtube"></i></a>
+                </div>
             </div>
         </div>
     </div>
